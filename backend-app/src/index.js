2abb941b4eb0f553c157dcb1fd55e2387f90b56e
EricSoder <70255364+EricSoder@users.noreply.github.com>
2022-04-05 17:33:38 +0200

    Added awilix connections to the files

--- backend-app/src/index.js
+++ backend-app/src/index.js
@@ -7,10 +7,13 @@ container.register({
     restRouter: awilix.asFunction(require('./api-layer/rest-router')),
 
     //logic-layer
-
+    mooverInterface: awilix.asFunction(require('./logic-layer/interfaces/moover-interface')),
 
     //data-layer
+    db: awilix.asFunction(require('./data-layer/database/database-init/db-init')),
+    mooverRepository: awilix.asFunction(require('./data-layer/database/database-access/moover-repository')),
 
+    //app
     app: awilix.asFunction(require('./api-layer/app'))
 })
 
