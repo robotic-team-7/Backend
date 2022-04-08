1a0320c33e7262a64b0b48dc37ae57f378962377
EricSoder <70255364+EricSoder@users.noreply.github.com>
2022-04-08 14:32:26 +0200

    Created CRUD operations for the mower table

--- backend-app/src/data-layer/database/database-access/mower-repository.js
+++ backend-app/src/data-layer/database/database-access/mower-repository.js
@@ -1,8 +1,10 @@
 /* To access mower table in database*/
 
 module.exports = function({ db }) {
+
     const exports = {}
 
+    /* To create a mower */
     exports.createMower = function(UserID, SerialNumber, Status, callback) {
 
         const mower = {
@@ -14,6 +16,49 @@ module.exports = function({ db }) {
         db.Mowers.create(mower)
             .then(createdMower => callback([], createdMower.MowerID))
             .catch(e => { callback(e, []) })
+
+    }
+
+
+
+    /* To update mower status */
+    exports.updateMowerStatus = function(MowerID, Status, callback) {
+
+        db.Mowers.update({ Status: Status }, {
+                where: { MowerID: MowerID },
+                returning: true,
+                raw: true
+            })
+            .then(mower => callback([], mower.Status))
+            .catch(e => {
+                console.log(e)
+                callback(e, [])
+            })
+
+    }
+
+
+
+    /* To delete a mower */
+    exports.deleteMower = function(MowerID, callback) {
+
+        db.Mowers.destroy({
+                where: { MowerID: MowerID },
+                raw: true
+            })
+            .then(numberOfDeletedMowers => {
+                /* If the mower dosn`t exists it returns false (because no mower has been deleted) otherwise true (because a mower has been deleted) */
+                if (numberOfDeletedMowers == 0) {
+                    callback([], false)
+                } else {
+                    callback([], true)
+                }
+            })
+            .catch(e => {
+                console.log(e)
+                callback(e, false)
+            })
+
 
     }
 
--- backend-app/src/logic-layer/interfaces/mower-interface.js
+++ backend-app/src/logic-layer/interfaces/mower-interface.js
@@ -2,6 +2,7 @@ module.exports = function({ mowerRepository }) {
 
     const exports = {}
 
+    /* To create a mower */
     exports.createMower = function(UserID, SerialNumber, Status, callback) {
 
         mowerRepository.createMower(UserID, SerialNumber, Status, function(error, MowerID) {
@@ -9,6 +10,36 @@ module.exports = function({ mowerRepository }) {
                 callback(error, [])
             } else {
                 callback([], MowerID)
+            }
+        })
+
+    }
+
+
+
+    /* To update mower status */
+    exports.updateMowerStatus = function(MowerID, Status, callback) {
+
+        mowerRepository.updateMowerStatus(MowerID, Status, function(error, newMowerStaus) {
+            if (Object.keys(error).length > 0) {
+                callback(error, [])
+            } else {
+                callback([], newMowerStaus)
+            }
+        })
+
+    }
+
+
+
+    /* To delete a mower */
+    exports.deleteMower = function(MowerID, callback) {
+
+        mowerRepository.deleteMower(MowerID, function(error, mowerDeleted) {
+            if (Object.keys(error).length > 0) {
+                callback(error, [])
+            } else {
+                callback([], mowerDeleted)
             }
         })
 
