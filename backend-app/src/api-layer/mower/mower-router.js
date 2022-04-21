e799bd24a057e183addd644591903acf779f7a59
Arvid <56995454+arvid95@users.noreply.github.com>
2022-04-21 15:34:44 +0200

    Updated mower-router
    I realized the mower will only have to update its status with the backend, so I removed all other functions.
    Also added proper status code handling.

--- backend-app/src/api-layer/mower/mower-router.js
+++ backend-app/src/api-layer/mower/mower-router.js
@@ -6,48 +6,6 @@ module.exports = function({ mowerInterface }) {
 
     const router = express.Router()
 
-    /* Retrieve Mower by MowerID */
-    router.get('/:mowerId', function(req, res) {
-
-        let MowerID = req.params.mowerId
-
-        /* Call mowerInterface to get mower by mowerId*/
-        mowerInterface.getMowerById(mowerId, function(error, mower) {
-
-            res.send(mower)
-        })
-    })
-
-    /* Retrieve all mowers by userId */
-    router.get('/:userId', function(req, res) {
-
-        let userId = req.params.userId
-
-        mowerInterface.getAllmMowersByUserId(userId, function(error, mowers) {
-
-            res.send(mowers)
-        })
-    })
-
-    /* Create new mower */
-    router.post('/', function(req, res) {
-
-        let userId = req.body.userId
-        let serialNumber = req.body.serialNumber
-        let status = req.body.status
-
-        /* Call mowerInterface to create new mower */
-        mowerInterface.createMower(userId, serialNumber, status, function(error, mowerId) {
-
-            if (error) {
-                res.send(error)
-            }
-            else {
-                res.redirect('/mowers/'+mowerId)
-            }
-        })
-    })
-
     /* Update mower status */
     router.put('/:mowerId', function(req, res) {
 
@@ -57,27 +15,11 @@ module.exports = function({ mowerInterface }) {
         /* Call mowerInterface to update mower status */
         mowerInterface.updateMowerStatus(mowerId, status, function(error, newMowerStatus) {
 
-            if (error) {
-                res.send(error)
+            if (error.length > 0 && mowerId == null) {
+                res.status(404).json(error)
             }
             else {
-                res.send(newMowerStatus)
-            }
-        })
-    })
-
-    /* Delete mower */
-    router.delete('/:mowerId', function(req, res) {
-
-        let mowerId = req.params.mowerId
-
-        /* Call mowerInterface to delete mower */
-        mowerInterface.deleteMower(mowerId, function(error, mowerDeleted) {
-            if (error) {
-                res.send(error)
-            }
-            else {
-                res.send(mowerDeleted)
+                res.status(200).json(newMowerStatus)
             }
         })
     })
