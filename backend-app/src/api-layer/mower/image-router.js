75fce9026e0aca0b3b747a92417ff69a2a9a2b7c
Arvid <56995454+arvid95@users.noreply.github.com>
2022-04-21 13:53:40 +0200

    Removed unnecessary functions from image-router

--- backend-app/src/api-layer/mower/image-router.js
+++ backend-app/src/api-layer/mower/image-router.js
@@ -6,22 +6,6 @@ module.exports = function({ mowerInterface }) {
 
     const router = express.Router()
 
-    /* Retrieve all images */
-    router.get('/', function(req, res) {
-
-
-        res.send("Images page!")
-
-    })
-
-    /* Retrieve an image by ImageID */
-    router.get('/imageId', function(req, res) {
-
-        let imageId = req.params.imageId
-
-        res.send("Image goes here...")
-    })
-
     /* Add new image */
     router.post('/', function(req, res) {
 
