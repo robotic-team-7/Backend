fd8ab06558181d8a06bd7a6776389cef1a556b04
EricSoder <70255364+EricSoder@users.noreply.github.com>
2022-04-24 10:33:38 +0200

    Added input validation for the methods in the mower interface

---
+++ backend-app/src/logic-layer/validation/mower-validation.js
@@ -0,0 +1,100 @@
+module.exports = function({}) {
+
+    const exports = {}
+
+    /* Validates parameters of createMower */
+    exports.createMowerValidation = function(userId, serialNumber, status) {
+
+        const validationErrors = []
+
+        // Check if the userId is an integer
+        if (Number.isInteger(userId) == false) {
+            validationErrors.push('invalidDataType')
+        }
+
+        // Check if the serialNumber is a string
+        if (typeof serialNumber != 'string') {
+            console.log(typeof serialNumber)
+            validationErrors.push('invalidDataType')
+        }
+
+        // Check if the status is a boolean
+        if (typeof status != 'boolean') {
+            console.log(typeof status)
+            validationErrors.push('invalidDataType')
+        }
+
+        return validationErrors
+    }
+
+
+
+
+    /* Validates parameters of getMowerByMowerId */
+    exports.getMowerByMowerIdValidation = function(mowerId) {
+
+        const validationErrors = []
+
+        // Check if the mowerId is an integer
+        if (Number.isInteger(mowerId) == false) {
+            validationErrors.push('invalidDataType')
+        }
+        return validationErrors
+    }
+
+
+
+
+    /* Validates parameters of getAllMowersByUserId */
+    exports.getAllMowersByUserIdValidation = function(userId) {
+
+        const validationErrors = []
+
+        // Check if the userId is an integer
+        if (Number.isInteger(userId) == false) {
+            validationErrors.push('invalidDataType')
+        }
+        return validationErrors
+
+    }
+
+
+
+
+    /* Validates parameters of updateMowerStatus */
+    exports.updateMowerStatusValidation = function(mowerId, status) {
+
+        const validationErrors = []
+
+        // Check if the mowerId is an integer
+        if (Number.isInteger(mowerId) == false) {
+            validationErrors.push('invalidDataType')
+        }
+
+        // Check if the status is a boolean
+        if (typeof status != 'boolean') {
+            console.log(typeof status)
+            validationErrors.push('invalidDataType')
+        }
+        return validationErrors
+    }
+
+
+
+
+    /* Validates parameters of deleteMower */
+    exports.deleteMowerValidation = function(mowerId) {
+
+        const validationErrors = []
+
+        // Check if the mowerId is an integer
+        if (Number.isInteger(mowerId) == false) {
+            validationErrors.push('invalidDataType')
+        }
+
+        return validationErrors
+    }
+
+
+    return exports
+}
