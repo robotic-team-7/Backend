7b30ce379402ef08d31f427fcb2903583ff0fae6
Arvid <56995454+arvid95@users.noreply.github.com>
2022-04-28 14:24:18 +0200

    Created image-router

--- backend-app/src/api-layer/mower/image-router.js
+++ backend-app/src/api-layer/mower/image-router.js
@@ -1,15 +1,17 @@
 /* Image router */
 
 const express = require("express")
+const multer = require("multer")
+const upload = multer()
 
 module.exports = function({ mowerInterface }) {
 
     const router = express.Router()
 
     /* Add new image */
-    router.post('/', function(req, res) {
+    router.post('/', upload.none(), function(req, res) {
 
-        return
+        
     })
 
     return router
