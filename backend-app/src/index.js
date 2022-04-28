44eab4668827353913a1e0f86b1668a1bb7bba83
EricSoder <70255364+EricSoder@users.noreply.github.com>
2022-04-28 13:20:09 +0200

    Update index.js

--- backend-app/src/index.js
+++ backend-app/src/index.js
@@ -9,19 +9,22 @@ container.register({
     mowingSessionRouter: awilix.asFunction(require('./api-layer/mower/mowing-session-router')),
     mobileRouter: awilix.asFunction(require('./api-layer/mobile/mobile-router')),
 
+
     //logic-layer
     mowerInterface: awilix.asFunction(require('./logic-layer/interfaces/mower-interface')),
     mowingSessionInterface: awilix.asFunction(require('./logic-layer/interfaces/mowing-session-interface')),
+    obstacleInterface: awilix.asFunction(require('./logic-layer/interfaces/obstacle-interface')),
     dbError: awilix.asFunction(require('./logic-layer/error-handling/db-error')),
     mowingSessionValidation: awilix.asFunction(require('./logic-layer/validation/mowing-session-validation')),
     mowerValidation: awilix.asFunction(require('./logic-layer/validation/mower-validation')),
-
 
 
     //data-layer
     db: awilix.asFunction(require('./data-layer/database/database-init/db-init')),
     mowerRepository: awilix.asFunction(require('./data-layer/database/database-access/mower-repository')),
     mowingSessionRepository: awilix.asFunction(require('./data-layer/database/database-access/mowing-session-repository')),
+    obstacleRepository: awilix.asFunction(require('./data-layer/database/database-access/obstacle-repository')),
+    googleVision: awilix.asFunction(require('./data-layer/vision-api/googleVision')),
 
     //app
     app: awilix.asFunction(require('./api-layer/app'))
