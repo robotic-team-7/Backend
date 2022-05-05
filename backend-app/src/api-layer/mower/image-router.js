90d030719611ccca4176d3f3a0634fdb53deb878
EricSoder <70255364+EricSoder@users.noreply.github.com>
2022-05-05 10:39:57 +0200

    added jwt and fs to file

--- backend-app/src/api-layer/mower/image-router.js
+++ backend-app/src/api-layer/mower/image-router.js
@@ -1,6 +1,12 @@
 /* Image router */
 
 const express = require("express")
+const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem')
+const fs = require('fs')
+const rawData = fs.readFileSync(process.env.JWK_KEY_PATH)
+const jwk = JSON.parse(rawData)
+const pem = jwkToPem(jwk.keys[1])
 
 module.exports = function({ mowerInterface }) {
 
